a27814023cc7c7a7c5f05c6fbd52169a32d29f0c
Delaine Wendling <delaine.wendling@gmail.com>
2016-08-19 10:54:38 -0500

    Added create checkerboard function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 
+function createCheckerboard () {
+  for (i=7; i>=0; i--) {
+    $(".checkerBoard").append("<div class='row'><div class='col-xs-2'></div>");
+    for (j=0; j<8; j++){
+      $(".checkerBoard").append("<div class='col-xs-1 hasRed hasBlack empty' x=j y=i><div class='piece'></div></div>")
+    }
+    $(".checkerBoard").append("<div class='col-xs-2'></div></div>")
+  }
+}
