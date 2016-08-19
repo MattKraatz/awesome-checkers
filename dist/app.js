8e228224f8a30171f01b2441dea1e8827fb8c772
Casey Reed <casey.w.reed@gmail.com>
2016-08-19 12:35:43 -0500

    Board styling and populating

--- dist/app.js
+++ dist/app.js
@@ -3,11 +3,11 @@
 function createCheckerboard () {
   var checkerboard = ""
   for (i=7; i>=0; i--) {
-      checkerboard += `<div class='row row-eq-height'><div class='col-xs-2'></div>`
+    checkerboard += `<div class='checkerRow'>`
     for (j=0; j<8; j++){
-      checkerboard += `<div class='col-xs-1 emptySpace' x=${j} y=${i}><div class='piece'></div></div>`
+      checkerboard += `<div class='emptySpace' x=${j} y=${i}><div></div></div>`
     }
-      checkerboard += `<div class='col-xs-2'></div></div>`
+    checkerboard += `</div>`
   }
   $(".checkerBoard").append(checkerboard)
 }
