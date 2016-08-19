628a102e29ca7b7199140dc57faca17d15b72538
Matt Kraatz <matt.kraatz@gmail.com>
2016-08-19 10:35:45 -0500

    boilerplate

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+  // Load grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-stylelint');
+  grunt.loadNpmTasks('grunt-jsonlint');
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  // Plugin configuration
+  grunt.initConfig({
+    browserify: {
+      './dist/app.js': ['./scripts.js']
+    },
+    jshint: {
+      all: {
+        src: ['./**/*.js', '!./dist/app.js', '!./node_modules/**/*', '!./bower_components/**/*', '!./Gruntfile.js'],
+        options: {
+          jshintrc: true
+        }
+      }
+    },
+    stylelint: {
+      all: {
+        src: ['./**/*.css', '!./node_modules/**/*', '!./bower_components/**/*']
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      js: {
+        files: ['./**/*.js', '!./dist/app.js','!./node_modules/**/*', '!./bower_components/**/*'],
+        tasks: ['browserify','jshint']
+      },
+      html: {
+        files: ['./**/*.html', '!./node_modules/**/*', '!./bower_components/**/*'],
+        tasks: ['htmlhint']
+      },
+      css: {
+        files: ['./**/*.css', '!./node_modules/**/*', '!./bower_components/**/*'],
+        tasks: ['stylelint']
+      },
+      json: {
+        files: ['./**/*.json', '!./node_modules/**/*', '!./bower_components/**/*'],
+        tasks: ['jsonlint']
+      }
+    },
+    jsonlint: {
+      all: {
+        src: ['./**/*.json', '!./node_modules/**/*', '!./bower_components/**/*'],
+        options: {
+          format: true,
+          indent: 2
+        }
+      }
+    },
+    htmlhint: {
+      all: {
+        src: ['./**/*.html', '!./node_modules/**/*', '!./bower_components/**/*'],
+        options: {
+          htmlhintrc: '.htmlhintrc'
+        }
+      }
+    }
+  });
+  grunt.registerTask('default', ['browserify','watch']);
+};
