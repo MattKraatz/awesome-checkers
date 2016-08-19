0c86dbe0f12ae5e3e346a94cb6e0ac6722443101
Casey Reed <casey.w.reed@gmail.com>
2016-08-19 11:27:46 -0500

    Checkerboard created with correct colors

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,13 @@
 function createCheckerboard () {
   var checkerboard = ""
   for (i=7; i>=0; i--) {
-      checkerboard += `<div class='row'><div class='col-xs-2'></div>`
+      checkerboard += `<div class='row row-eq-height'><div class='col-xs-2'></div>`
     for (j=0; j<8; j++){
-      checkerboard += `<div class='col-xs-1 redPiece blackPiece empty' x=${j} y=${i}><div class='piece'></div></div>`
+      checkerboard += `<div class='col-xs-1 emptySpace' x=${j} y=${i}><div class='piece'></div></div>`
     }
       checkerboard += `<div class='col-xs-2'></div></div>`
   }
   $(".checkerBoard").append(checkerboard)
 }
+
+createCheckerboard()
