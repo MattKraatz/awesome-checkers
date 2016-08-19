ce0dc657998ae82dffc03cccfc30d905b6fd8941
Matt Hamil <j.matthew.hamil@gmail.com>
2016-08-19 13:53:30 -0500

    Added setNode, changeCoords to Piece prototype

--- js/pieces.js
+++ js/pieces.js
@@ -4,6 +4,7 @@ var Piece = function(){
   this.canJump = false;
   this.canBeJumped = false;
   this.color = null;
+  this.node = null;
 }
 Piece.prototype.jump = function(){
 
@@ -11,21 +12,31 @@ Piece.prototype.jump = function(){
 Piece.prototype.remove = function(){
 
 }
-Piece.prototype.getCoordinates = function(){
-  
+Piece.prototype.setNode = function() {
+  this.node = $(`[x=${this.x}][y=${this.y}]`);
+}
+
+/**
+ * Changes the coordinates of this piece
+ * @param  {Integer} x X coordinate of piece
+ * @param  {Integer} y Y coordinate of piece
+ */
+Piece.prototype.changeCoords = function(x, y) {
+  this.x = x
+  this.y = y
+  this.setNode()
 }
 
 var RedPiece = function (x, y){
   this.color = "red";
-  this.x = x;
-  this.y = y;
+  this.changeCoords(x, y);
+
 }
 RedPiece.prototype = new Piece();
 
 var BlackPiece = function (x, y){
   this.color = "black";
-  this.x = x;
-  this.y = y;
+  this.changeCoords(x, y);
 }
 BlackPiece.prototype = new Piece();
 
